d853e4f81b8ad6c3bb201c960698dfd06daaa594
fillipiconforti <fillipi@gmail.com>
2026-05-31 12:14:30 -0300

    corrige porta

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 const buscaRouter = require('./src/routes/busca');
 app.use('/busca', buscaRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
